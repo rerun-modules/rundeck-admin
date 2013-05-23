36c00d32676fd8afed794b2a456c14cfc6cbc0a8
Alex Honor <alexhonor@yahoo.com>
2013-05-22 20:18:01 -0700

    added log messages to build script.

--- travis-ci.sh
+++ travis-ci.sh
@@ -16,7 +16,7 @@ VERSION=$(awk -F= '/VERSION/ {print $2}' metadata)
 [ -z "${VERSION:-}" ] && {
     echo >&2 "ERROR: Version info could not be found in metadata file."; exit 1
 }
-
+echo "Building version $VERSION..."
 # Create a scratch directory and change directory to it.
 WORK_DIR=$(mktemp -d "/tmp/build-$MODULE.XXXXXX")
 
@@ -24,6 +24,7 @@ WORK_DIR=$(mktemp -d "/tmp/build-$MODULE.XXXXXX")
 # ---------
 
 # Get rerun so we have access to stubbs:archive command.
+echo "Getting rerun..."
 pushd $WORK_DIR
 git clone git://github.com/rerun/rerun.git rerun
 # chdir to original directory where module this module source resides.
@@ -34,13 +35,14 @@ export RERUN_MODULES=$WORK_DIR/rerun/modules
 export RERUN=$WORK_DIR/rerun/rerun
 
 # Add bintray module into the $RERUN_MODULES directory
+echo "Getting bintray module..."
 pushd $RERUN_MODULES
 git clone git://github.com/rerun-modules/bintray.git bintray
 popd
 
 # Build the module.
 # -----------------
-
+echo "Packaging the build..."
 # Copy the module files into the work directory.
 mkdir -p $RERUN_MODULES/$MODULE
 echo "copying $MODULE files into working directory..."
@@ -55,8 +57,11 @@ $RERUN stubbs:archive --modules $MODULE
 }
 
 # Upload and publish to bintray
+echo "Uploading the build to bintray: /${BINTRAY_ORG}/${BINTRAY_REPO}/${MODULE}/${VERSION}..."
 $RERUN bintray:package-upload \
     --user ${BINTRAY_USER} --apikey ${BINTRAY_APIKEY} \
     --org ${BINTRAY_ORG}   --repo ${BINTRAY_REPO} \
     --package $MODULE      --version $VERSION \
     --file rerun.bin
+
+echo "Done."
