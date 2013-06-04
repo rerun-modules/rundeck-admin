776e3872a1becdf712232c0b6fd6e0f65b303ba0
Alex Honor <alexhonor@yahoo.com>
2013-06-03 18:09:32 -0700

    changed -data to -model paramater

--- tests/resource-add-1-test.sh
+++ tests/resource-add-1-test.sh
@@ -20,12 +20,12 @@ rerun rundeck-admin:project-create $CREDS --project test-$$
 # ------
 
 it_creates_a_resource() {
-    rerun rundeck-admin:resource-add $CREDS --project test-$$ --data "-name gilligan -description 'silly sailor'"
+    rerun rundeck-admin:resource-add $CREDS --project test-$$ --model "-name gilligan -description 'silly sailor'"
 }
 
 it_updates_a_resource() {
     CREDS="--user admin --password admin --url http://localhost:4440"
-    rerun rundeck-admin:resource-add $CREDS --project test-$$ --data "-name gilligan -description 'silly sailor' -osFamily comedian -blar blar"
+    rerun rundeck-admin:resource-add $CREDS --project test-$$ --model "-name gilligan -description 'silly sailor' -osFamily comedian -blar blar"
 }
 
 # ------------------------------
